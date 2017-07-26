0c9fc9a1956e7ad6f93233d1abc2d66c2df1ea8f
Anthony Robinson <robinsonamk@gmail.com>
2017-07-26 10:19:04 -0700

    finished the last to dos

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -4,7 +4,6 @@ var app = app || {};
 (function(module) {
   const repos = {};
   repos.all = [];
-  console.log(repos.all);
 
   repos.requestRepos = function(callback) {
     $.ajax({
@@ -14,30 +13,34 @@ var app = app || {};
         Authorization: `token ${GITHUB_TOKEN}`
       }
     })
-    .then(
-      data => {
-        console.log(data);
-        $.get(data.repos_url).then(console.log);
+    .then(data => {
+      return $.get(data.repos_url);
+    })
+    .then(data => {
+      callback(null, data);
+    })
+    .catch(err => {
+      callback(err);
+      console.error('Failed to load repos: ', err);
+    });
 
-    // Object.keys(data).forEach(line => $('#results').append(`
-    //     <li>${line}: ${data[line]}</li>
-    //   `)
-    // )
-      },
-      err => {
-        console.error(err);
-      }
-);
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
-    repos.all = repos.requestRepos(callback);
+
   };
+
+  repos.requestRepos((err, data) => {
+    if (err) console.log(err);
+    repos.all = data;
+    console.log('repos: ', repos.all);
+  });
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
   repos.with = attr => repos.all.filter(repo => repo[attr]);
+  // let reposWithProjects = repos.with('has_projects');
 
   module.repos = repos;
 })(app);
